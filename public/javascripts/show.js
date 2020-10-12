19f42b4707d1e5c6e5d5c2a07fcc03dab73ac3dd
nullpo299 <nullpointer299@gmail.com>
2020-10-12 23:23:49 +0900

    add dropdown select number

--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -1,42 +1,8 @@
 $(function () {
+    $('[data-toggle="tooltip"]').tooltip();
 
-    $("#btnCreate").click(function () {
-
-    })
-
-    $("#btnUpdate").click(function () {
-
-    })
-
-    $("#btnDelete").click(function () {
-
-    })
-
-    $('#createModal').on('show.bs.modal', function (event) {
-        const button = $(event.relatedTarget);
-        const recipient = button.data('whatever');
-
-        const modal = $(this);
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
-    })
-
-    $('#updateModal').on('show.bs.modal', function (event) {
-        const button = $(event.relatedTarget);
-        const recipient = button.data('whatever');
-
-        const modal = $(this);
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
-    })
-
-    $('#deleteModal').on('show.bs.modal', function (event) {
-        const button = $(event.relatedTarget);
-        const recipient = button.data('whatever');
-
-        const modal = $(this);
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
-    })
-
-})
+    $('.dropdown-menu .dropdown-item').click(function () {
+        const visibleItem = $('.dropdown-toggle', $(this).closest('.dropdown'));
+        visibleItem.text($(this).attr('value'));
+    });
+});
